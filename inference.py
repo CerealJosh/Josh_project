5bb8064bc1cdbe3ae46b0437c77d7b93159e535d
Onu Joshua <onujoshua2001@gmail.com>
2024-08-28 14:05:52 +0100

    Sensor fixes

--- inference.py
+++ inference.py
@@ -2,12 +2,17 @@ import joblib
 import tensorflow as tf
 import numpy as np
 
+data_path = 'C:/Users/Josh/source/repos/ProjectServer/Josh-main/data.json'
+labels_path = 'C:/Users/Josh/source/repos/ProjectServer/Josh-main/labels.txt'
 labels = []
-with open('labels.txt','r') as f:
+with open(labels_path,'r') as f:
     for f in f.readlines():
         labels.append(f.split('\t')[1].strip())
-
-
+with open(data_path, 'r') as g:
+        Z = g.read().strip()
+        Z= Z.strip("[]")
+        Z = Z.split(",")
+print(Z)
 files_list = ['logistic_regression.pkl',
               'decision_tree.pkl',
               'k_neighbors.pkl',
@@ -32,17 +37,19 @@ def get_predictions(model_num,features):
 features = []
 feature_list = ['CO_Room','H2_Room','Humidity_Room','Temperature_Room','VOC_Room_RAW']
 for i in range(5):
-    features.append(float(input(f'Enter the {feature_list[i]} value\t')))
+    features.append(Z[i])
 
+print(features)
+# model_num = int(input(f'Which model to use......\n\
+#                 1.Logistic Regression\n\
+#                 2.Decision Tree\n\
+#                 3.KNeighbors\n\
+#                 4.Random Forest\n\
+#                 5.Gradient Boosting\n\
+#                 6.Extreme Gradient Boosting\n\
+#                 7.Stacking\n\
+#                 8.Neural Network\n'))
 
-model_num = int(input(f'Which model to use......\n\
-                1.Logistic Regression\n\
-                2.Decision Tree\n\
-                3.KNeighbors\n\
-                4.Random Forest\n\
-                5.Gradient Boosting\n\
-                6.Extreme Gradient Boosting\n\
-                7.Stacking\n\
-                8.Neural Network\n'))
+model_num = 4
 
 print(get_predictions(model_num,features))
